d3f94bd17e6c7d8686650a665e2d68b7ab362a90
vyacheslavmartynenko <evilacer5755g@gmail.com>
2017-07-31 00:39:15 +0300

    basic configuration added

--- app/src/main/java/com/finder/voroshilo/activity/MainActivity.java
+++ app/src/main/java/com/finder/voroshilo/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.finder.voroshilo;
+package com.finder.voroshilo.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.finder.voroshilo.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
---
+++ app/src/main/java/com/finder/voroshilo/application/FinderApplication.java
@@ -0,0 +1,6 @@
+package com.finder.voroshilo.application;
+
+import android.support.multidex.MultiDexApplication;
+
+public class FinderApplication extends MultiDexApplication {
+}
