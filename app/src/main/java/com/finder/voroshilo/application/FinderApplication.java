858edf21a9d66b2baf1fd3471bd25a208b8cb1d4
vyacheslavmartynenko <evilacer5755g@gmail.com>
2017-07-31 01:45:59 +0300

    basic configuration added

--- app/src/main/java/com/finder/voroshilo/application/FinderApplication.java
+++ app/src/main/java/com/finder/voroshilo/application/FinderApplication.java
@@ -2,5 +2,29 @@ package com.finder.voroshilo.application;
 
 import android.support.multidex.MultiDexApplication;
 
+import com.finder.voroshilo.activity.BaseActivity;
+
 public class FinderApplication extends MultiDexApplication {
+    private static FinderApplication instance;
+    private BaseActivity currentActivity = null;
+
+    public static synchronized FinderApplication getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //Fabric.with(this, new Crashlytics());
+        //SharedPreferencesProvider.getInstance().initialize(getApplicationContext());
+        instance = this;
+    }
+
+    public BaseActivity getCurrentActivity() {
+        return currentActivity;
+    }
+
+    public void setCurrentActivity(BaseActivity currentActivity) {
+        this.currentActivity = currentActivity;
+    }
 }
