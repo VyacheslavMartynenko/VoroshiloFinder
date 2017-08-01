181c6abd094bd78e70aa900a6bffb26a066a23a2
vyacheslavmartynenko <evilacer5755g@gmail.com>
2017-08-02 01:43:42 +0300

    add applications request

---
+++ app/src/main/java/com/finder/voroshilo/networking/request/ApplicationsRequest.java
@@ -0,0 +1,43 @@
+package com.finder.voroshilo.networking.request;
+
+import android.support.annotation.NonNull;
+
+import com.finder.voroshilo.model.networking.data.DataBody;
+import com.finder.voroshilo.model.networking.data.MainDataBody;
+import com.finder.voroshilo.networking.ApiBuilder;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ApplicationsRequest {
+    public interface ApplicationCallback {
+        void onSuccess(DataBody data);
+
+        void onError(Throwable throwable);
+    }
+
+    public void requestSettings(final ApplicationsRequest.ApplicationCallback callback) {
+        ApiBuilder.getApiService().getData().enqueue(new Callback<MainDataBody>() {
+            @Override
+            public void onResponse(@NonNull Call<MainDataBody> call, @NonNull Response<MainDataBody> response) {
+                if (response.isSuccessful()) {
+                    MainDataBody mainDataBody = response.body();
+                    if (mainDataBody != null) {
+                        DataBody dataBody = mainDataBody.getData();
+                        callback.onSuccess(dataBody);
+                    } else {
+                        callback.onError(new NullPointerException());
+                    }
+                } else {
+                    callback.onError(new Exception(String.valueOf(response.code())));
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<MainDataBody> call, @NonNull Throwable t) {
+                callback.onError(t);
+            }
+        });
+    }
+}
